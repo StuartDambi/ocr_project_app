7b0c4d48da6f404ffef762692f91cd4be5325c9f
StuartDambi <stuartdambi@gmail.com>
2019-08-02 13:47:33 +0300

    Made a Splash Screen and introduction session to teach user how to use the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,16 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:ocr_app/pages/home_screen.dart';
+import 'package:ocr_app/pages/intro_screen.dart';
+import 'package:ocr_app/pages/splash_screen.dart';
 
-void main() => runApp(MaterialApp(
+var routes = <String, WidgetBuilder>{
+  "/home": (BuildContext context) => HomeScreen(),
+  "/intro": (BuildContext context) => IntroScreen(),
+};
+
+void main() => runApp(new MaterialApp(
     theme:
         ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
     debugShowCheckedModeBanner: false,
     home: SplashScreen(),
-));
-
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-    void initState() {
-      super.initState();
-      Timer(Duration(seconds: 5), () => print('Welcome to the OCR Project!'));
-      // if (5 == Timer){
-      //   print('Application is taking longer than Usual to start');
-      // }
-    }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.redAccent),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: 50.0,
-                        child: Icon(
-                          Icons.satellite,
-                          color: Colors.greenAccent,
-                          size: 50.0,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(top: 10.0),
-                      ),
-                      Text(
-                        "My OCR",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24.0, 
-                          fontWeight: FontWeight.bold
-                          ),
-                      )
-                    ],
-                  ),
-                )
-              ),
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20.0),
-                    ),
-                    Text(
-                      "Text Coverter\n for Everyone",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold
-                        ),
-                    )
-                  ],
-                ),
-                )
-            ],
-            )
-        ],
-      ),
-    );
-  }
-}
+    routes: routes));
---
+++ lib/utils/flutkart.dart
@@ -0,0 +1,19 @@
+class Flutkart {
+  static const String name = "OCR Project";
+  static const String store = "Scan Handwriting\n For Everyone";
+  static const String wt1 = "WELCOME";
+  static const String wc1 =
+      "MyCharacter is premium\n Platform for Everyone";
+  static const String wt2 = "Discover Handwriting";
+  static const String wc2 =
+      "Search Latest and Featured Product\n With Best Price";
+  static const String wt3 = "Scan your Document";
+  static const String wc3 =
+      "Place your document below camera\n And focus phone";
+  static const String wt4 = "Convert Document";
+  static const String wc4 =
+      "Your print will appear in a while";
+  static const String skip = "SKIP";
+  static const String next = "NEXT";
+  static const String gotIt = "GOT IT";
+}
